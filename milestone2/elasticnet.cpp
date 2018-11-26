cf3157708a83bdcea8833a570be2874ede6c9e79
zeynepyvs <44851660+zeynepyvs@users.noreply.github.com>
2018-11-26 11:14:52 +0100

    Update elasticnet.cpp

--- milestone2/elasticnet.cpp
+++ milestone2/elasticnet.cpp
@@ -1,6 +1,78 @@
 #include "elasticnet.h"
+#include <istream>
+#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <math.h>
+#define PI 3.14159265
+
+
+using namespace std;
+
+
+void ElasticNet :: addCity(){
+    int y=0;
+    int z=0;
+    City x;
+    cout << "Please enter the coordinates of the city.";
+    cin >> (y, z);
+    x.coord = position (y,z);
+    cities.push_back(x);
+    first_coord.push_back(y);
+    second_coord.push_back(z);
+    
+
+
+}
+
+
+void ElasticNet :: setCentroid(){
+    int x = 0,y = 0,counterx = 0,countery = 0, r;
+    vector<int>::iterator it;
+
+    for(it = first_coord.begin();it != first_coord.end(); it++){
+        x += *it;
+        counterx++;
+        r = *it;
+    }
+
+    for(it = second_coord.begin();it != second_coord.end(); it++){
+        y += *it;
+        countery++;
+    }
+    centroid = position (x/counterx, y/countery);
+
+}
+
+void ElasticNet :: addNode(){
+    double x = 0;
+    double y = 0;
+    double gamma = (2 * PI)/numOfNodes;
+    // for gamma := rotation angle in radians
+    for (int i = numOfNodes;i < numOfNodes; i++){
+        if(i == 0){
+            Node i;
+            x = centroid[0]+radius;
+            y= 0;
+            i.coord = position(x, y);
+            nodes.push_back(i);
+        }else{
+            Node i;
+            x += radius * cos(y + gamma);
+            y += radius * sin(gamma);
+            i.coord = position();
+            nodes.push_back(i);
+            //(x + radius*cos(theta + gamma), y + radius*sin(theta? + gamma))
+        }
+    }
+}
+
+void ElasticNet :: setNumOfNodes(){
+    numOfNodes = getNumOfCities() * getCvRatio();
+}
 
 ElasticNet::ElasticNet()
 {
+
 
 }
