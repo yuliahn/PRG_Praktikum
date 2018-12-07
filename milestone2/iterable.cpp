aab6a8b607280a0575eff6a9cadd1065ab19d9db
yuliahn <38583886+yuliahn@users.noreply.github.com>
2018-12-07 15:21:37 +0100

    Update iterable.cpp

--- milestone2/iterable.cpp
+++ milestone2/iterable.cpp
@@ -3,7 +3,7 @@
 
 #include <iostream>
 
-Iterable::Iterable(ElasticNet net, double k0, double alpha, double beta) {
+Iterable::Iterable(ElasticNet *net, double k0, double alpha, double beta) {
     this->net = net;
     this->k0 = k0;
     this->alpha = alpha;
@@ -24,11 +24,11 @@ void Iterable::updateT()
 void Iterable::updateV()
 {
     v.clear();
-    for (unsigned i=0; i<net.getNumOfCities(); i++) {     // for each city
+    for (unsigned i=0; i<net->getNumOfCities(); i++) {     // for each city
         vector <double> distances;                          // saves all euclidian distances between city i and all nodes
         double sum = 0;                                     // saves the sum of all exp(...) in denominator (of city i and all nodes)
-        for (unsigned a=0; a<net.getNumOfNodes(); a++) {
-            double distance = pow((euclDistance(net.getCities()[i], net.getNodes()[a])),2);
+        for (unsigned a=0; a<net->getNumOfNodes(); a++) {
+            double distance = pow((euclDistance(net->getCities()[i], net->getNodes()[a])),2);
             double expValue = exp( (-1) * distance / t);    // calculate the value of an exponent function for (i,a)
 //            cout << "City: " << i << ", Distance: " << distance << ", T: " << t << ", Value: " << expValue << endl;
             distances.push_back(expValue);                  // add the value to the distances vector
@@ -55,43 +55,43 @@ double Iterable::apply(int currentIteration)
     updateT();
     cout << "K: " << k << ", T: " << t << endl;
     updateV();
-    for (unsigned a=0; a < net.getNodes().size(); a++) {
+    for (unsigned a=0; a < net->getNodes().size(); a++) {
 //        cout << "Iteration " << a << ". Cities: " << net.getCities().size() << endl;
         vector <double> deltaA;                             // new delta generated in a loop for each node a
         vector <double> sum = {0, 0};                       // sum in the delta term that gets multiplied with alpha
-        for (unsigned i=0; i < net.getCities().size(); i++) {
+        for (unsigned i=0; i < net->getCities().size(); i++) {
 
-            sum = add (sum, multiply( getV()[i][a], subtract(net.getCities()[i].coord, net.getNodes()[a].coord)));
+            sum = add (sum, multiply( getV()[i][a], subtract(net->getCities()[i].coord, net->getNodes()[a].coord)));
         }
 
 //        cout << "Sum: " << sum[0] << ", " << sum[1] << endl;
 
         vector <double> distance = subtract (               // difference in the delta term that gets multiplied with beta and K
                     add (
-                        net.getNodes()[(a-1)%net.getNodes().size()].coord,
-                        net.getNodes()[(a+1)%net.getNodes().size()].coord
+                        net->getNodes()[(a-1)%net->getNodes().size()].coord,
+                        net->getNodes()[(a+1)%net->getNodes().size()].coord
                     ),
                     multiply (
-                        2, net.getNodes()[a].coord
+                        2, net->getNodes()[a].coord
                     )
         );
         deltaA = add ( multiply (alpha, sum), multiply (k*beta, distance));
         cout << "delta x: " << deltaA[0] << ", delta y: " << deltaA[1] << endl;
         // Update coordinates:
-        cout << "Old node coords, x: " << net.getNodes()[a].coord[0] << ", y: " << net.getNodes()[a].coord[1] << endl;
-        vector <double> values = {net.getNodes()[a].coord[0] + deltaA[0], net.getNodes()[a].coord[1] + deltaA[1]};
-        net.updateCoord(a, values);
-        cout << "New node coords, x: " << net.getNodes()[a].coord[0] << ", y: " << net.getNodes()[a].coord[1] << '\n' << endl;
+        cout << "Old node coords, x: " << net->getNodes()[a].coord[0] << ", y: " << net->getNodes()[a].coord[1] << endl;
+        vector <double> values = {net->getNodes()[a].coord[0] + deltaA[0], net->getNodes()[a].coord[1] + deltaA[1]};
+        net->updateCoord(a, values);
+        cout << "New node coords, x: " << net->getNodes()[a].coord[0] << ", y: " << net->getNodes()[a].coord[1] << '\n' << endl;
 //        cout << "NodeCoordX: " << net.getNodeCoordX(a) << endl;
 //        cout << "NodeCoordX: " << net.getNodeCoordY(a) << endl;
     }
 
     //find etaN:
     double etaN = 0; // find max (minDistance) across all cities, set minDistance to 0
-    for (unsigned i=0; i < net.getCities().size(); i++) {
+    for (unsigned i=0; i < net->getCities().size(); i++) {
         double minCity = INFINITY; // find min within each city, set minCity as infinity
-        for (unsigned a=0; a < net.getNodes().size(); a++) {
-            double distance = euclDistance(net.getCities()[i], net.getNodes()[a]);
+        for (unsigned a=0; a < net->getNodes().size(); a++) {
+            double distance = euclDistance(net->getCities()[i], net->getNodes()[a]);
 //            cout << "Node: " << a << ", Distance: " << distance << endl;
             if (isnan(distance)) break;
             minCity = (distance < minCity) ? (distance) : (minCity);
