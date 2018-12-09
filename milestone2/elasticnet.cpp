96d06028e44a1c437eabd52ced960c8a345829b2
zeynepyvs <44851660+zeynepyvs@users.noreply.github.com>
2018-12-09 16:14:42 +0100

    Update elasticnet.cpp

--- milestone2/elasticnet.cpp
+++ milestone2/elasticnet.cpp
@@ -11,12 +11,14 @@
 using namespace std;
 
 ElasticNet :: ElasticNet(double radius, double cvRatio) {
+	//function to define radius and cv Ratio of the elastic net
     this->radius = radius;
     this->cvRatio = cvRatio;
 }
 
 
 void ElasticNet :: addCity(double y,double z){
+	//function to add a city
     City x;
 
     int counter = 0;
@@ -38,6 +40,7 @@ void ElasticNet :: addCity(double y,double z){
 }
 
 void ElasticNet :: setCentroid(){
+	//function to determine the centroid of the cities (and nodes of the elastic net)
     double x = 0,y = 0,counterx = 0,countery = 0, r;
 
     for(int i = 0; i < cityCoordX.size(); i++){
@@ -56,6 +59,7 @@ void ElasticNet :: setCentroid(){
 }
 
 void ElasticNet :: addNodes(){
+	//function to add nodes
     double x = 0;
     double y = 0;
     double theta = 0;
@@ -68,7 +72,6 @@ void ElasticNet :: addNodes(){
         if(i == 0){
             Node i;
             x = centroid0 + radius;
-            //x = centroid0+radius*100;
             y = centroid1;
             i.coord = {x, y};
             nodes.push_back(i);
@@ -79,8 +82,6 @@ void ElasticNet :: addNodes(){
             theta += gamma;
             x += (radius) * cos(theta);
             y += (radius) * sin(theta);
-            //x += (radius*100) * cos(theta);
-            //y += (radius*100) * sin(theta);
             i.coord = {x,y};
             nodes.push_back(i);
             nodeCoordX.push_back(x);
@@ -90,10 +91,12 @@ void ElasticNet :: addNodes(){
 }
 
 void ElasticNet :: setNumOfNodes(){
+	//function to calculate the number of nodes
     numOfNodes = getNumOfCities() * getCvRatio();
 }
 
 void ElasticNet :: updateCoord(int i, vector <double> values) {
+	//function to update the x and y coordinates of nodes
     nodes[i].coord = values;
     nodeCoordX[i] = values[0];
     cout << "NodeCoordX: " << nodeCoordX[i] << endl;
@@ -102,6 +105,8 @@ void ElasticNet :: updateCoord(int i, vector <double> values) {
 }
 
 void ElasticNet :: preprocess(){
+	/*function to not recalculate the coordinates of nodes when the distance 
+	of the nodes to the nearest city is less than a predetermined limit "nZiel"*/
     for (int n = 0; n < nodes.size(); n++)
         for(int m = 0; m < cities.size(); m++){
             double coordx = cityCoordX[m] - nodeCoordX[n];
@@ -114,12 +119,14 @@ void ElasticNet :: preprocess(){
 }
 
 void ElasticNet :: clearCities() {
+	//function to clear cities
     cities.clear();
     cityCoordX.clear();
     cityCoordY.clear();
 }
 
 void ElasticNet :: clearNodes() {
+	//function to clear nodes
     nodes.clear();
     nodeCoordX.clear();
     nodeCoordY.clear();
