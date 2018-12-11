616613f15d402d5e1056323c94fbecc525552b28
yuliahn <38583886+yuliahn@users.noreply.github.com>
2018-12-11 16:33:50 +0100

    Update iterator.cpp

--- milestone2/iterator.cpp
+++ milestone2/iterator.cpp
@@ -1,3 +1,12 @@
+/**
+ * iterator.cpp
+ * Purpose: Iterate recalculation of elastic net until
+ * a maximum number of iterations or acceptable distance
+ * between nodes and cities is reached.
+ *
+ * @author Yulia Kim
+ */
+
 #include "iterator.h"
 
 Iterator::Iterator(const unsigned iterMax,
@@ -9,11 +18,15 @@ Iterator::Iterator(const unsigned iterMax,
     this->iterable = iterable;
 }
 
+/**
+ * @brief Iterator::solve
+ * Iterates recalculation of elastic net.
+ */
 void Iterator::solve()
 {
     for (unsigned i=0; i < iterMax; i++) {
         double etaN = iterable.apply(i);
-        if (etaN <= etaZiel) { // gewuenschte Genauigkeit erfuellt anstatt condition
+        if (etaN <= etaZiel) {
             break;
         } else {
             updateIter();
