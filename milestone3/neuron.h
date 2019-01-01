73e32ec803e3365c829845a9af8e190674344665
yuliahn <38583886+yuliahn@users.noreply.github.com>
2019-01-01 22:28:58 +0100

    Update neuron.h

--- milestone3/neuron.h
+++ milestone3/neuron.h
@@ -2,6 +2,8 @@
 #define NEURON_H
 
 #include <vector>
+#include <ctime>
+#include <cstdlib>
 
 using namespace std;
 
@@ -9,36 +11,35 @@ class Neuron
 {
 public:
     Neuron();
+    Neuron(double x);
+
+    void setValue(double x);
 
     // sets both activateValue and derivative, since they are interrelated
-    void setActivate(double x, double y) {
-        this->value = x;
-        this->derivative = y;
-    }
+    void setActivation(double x);
+    void setDerivation(double x);
+
+    double getValue();
+    double getActivationOutput();
+    double getDerivative();
 
 private:
     double value; // x_j = sum(incoming weights*neurons' values) value
-    double activateValue; // a_j = f(x_j) value
+    double activationOutput; // a_j = f(x_j) value
     double derivative; // f(x_j)' value
 };
 
+/* Examples of vectors for weights matrix with weights[layer2][layer1]
 vector <vector <double>> weights; // matrix of weights between two layers with neurons
 vector <Neuron> layer1; // vector of neurons in one layer
 vector <Neuron> layer2; // vector of neurons in one layer
+*/
 
-double activateFct(double x); // calculates activation function value for x
+// Aufgabe 1b)
+//use in main.cpp: neuron.setActivation(activationFct(neuron.getValue), derivation(neuron.getValue))
+double activationFct(double x); // calculates activation function value for x
 double derivation(double x); // calculates derivative of the activation function for x
 
-// generate/update the matrix with weights between neurons across layers
-void setWeights(vector <Neuron> layer1, vector <Neuron> layer2);
 
-// get weight of neuron i from layer1 to neuron j from layer2
-double getWeight(int i, int j, vector <vector <double>> weights);
-
-// returns a matrix with hidden layer gradients (weights between hiddenLayer1 and hiddenLayer2)
-vector <vector <double>> hiddenGradient(double eta, vector <vector <double>> weights, vector <Neuron> hiddenLayer1, vector <Neuron> hiddenLayer2);
-
-// returns a matrix with output layer gradients (weights between hiddenLayer and outputLayer)
-vector <vector <double>> outputGradient(double eta, vector <vector <double>> weights, vector <Neuron> hiddenLayer, vector <Neuron> outputLayer);
 
 #endif // NEURON_H
