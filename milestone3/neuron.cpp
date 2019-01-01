59b04b88adf6c00847e5c1d0b1d5005f378a0906
yuliahn <38583886+yuliahn@users.noreply.github.com>
2019-01-01 22:28:54 +0100

    Update neuron.cpp

--- milestone3/neuron.cpp
+++ milestone3/neuron.cpp
@@ -1,6 +1,41 @@
 #include "neuron.h"
 
-Neuron::Neuron()
-{
+Neuron::Neuron() {}
 
+Neuron::Neuron(double x)
+{
+    this->value = x;
+    this->activationOutput = 0;
+    this->derivative = 0;
 }
+
+void Neuron::setValue(double x)
+{
+    this->value = x;
+}
+
+void Neuron::setActivation(double x)
+{
+    this->activationOutput = x;
+}
+
+void Neuron::setDerivation(double x)
+{
+    this->derivative = x;
+}
+
+double Neuron::getValue()
+{
+    return this->value;
+}
+
+double Neuron::getActivationOutput()
+{
+    return this->activationOutput;
+}
+
+double Neuron::getDerivative()
+{
+    return this->derivative;
+}
+
