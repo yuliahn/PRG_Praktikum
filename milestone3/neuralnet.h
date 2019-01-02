70c6ff986fcef841ed239ed9d056e11a1f9d1538
yuliahn <38583886+yuliahn@users.noreply.github.com>
2019-01-02 11:02:43 +0100

    Update neuralnet.h

--- milestone3/neuralnet.h
+++ milestone3/neuralnet.h
@@ -1,11 +1,19 @@
 #ifndef NEURALNET_H
 #define NEURALNET_H
 
+#include "neuron.h"
+#include "weights.h"
 
 class NeuralNet
 {
 public:
     NeuralNet();
+    NeuralNet(vector <int> topology);
+
+private:
+    vector <int> topology;
+    vector <Weights> net;
+    vector <vector <Neuron>> layers;
 };
 
 #endif // NEURALNET_H
