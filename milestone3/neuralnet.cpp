278b202acc1633f0d87455b67ab0ab55fc83b9e9
yuliahn <38583886+yuliahn@users.noreply.github.com>
2019-01-02 11:02:21 +0100

    Update neuralnet.cpp

--- milestone3/neuralnet.cpp
+++ milestone3/neuralnet.cpp
@@ -4,3 +4,29 @@ NeuralNet::NeuralNet()
 {
 
 }
+
+NeuralNet::NeuralNet(vector <int> topology)
+{
+    // generate layers of neurons from topology
+    for (int i = 0; i < topology.size(); i++) {
+        // generate neurons using topology[i] as a number of neurons in each layer
+        vector <Neuron> layer;
+        for (int j = 0; j < topology[i]; j++) {
+            Neuron neuron;
+            layer.push_back(neuron);
+        }
+        layers.push_back(layer);
+    }
+
+    // add bias neurons to all layers but output layer
+    for (int i = 0; i < layers.size() - 1; i++) {
+        Neuron neuron(1); // bias equal to 1
+        layers[i].push_back(neuron);
+    }
+
+    // connect layers with randomly generated weights
+    for (int i = 0; i < layers.size() - 1; i++) {
+        Weights weights(layers[i], layers[i+1]);
+        net.push_back(weights);
+    }
+}
