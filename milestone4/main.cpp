5a142525f6d42168cb0c98e7d2daa30624ba52be
yuliahn <38583886+yuliahn@users.noreply.github.com>
2019-01-21 14:19:33 +0100

    Canvas added to mainwindow.

--- milestone4/main.cpp
+++ milestone4/main.cpp
@@ -15,9 +15,6 @@ int main(int argc, char *argv[])
     w.show();
 
 
-    vector <int> test = {};
-    cout << test.size() << endl;
-
     const char* imagesFile = "C:\\Users\\Yulia\\Documents\\Informatik\\WS18-19\\PRG_Praktikum\\milestones\\build-milestone4-Desktop_Qt_5_9_2_MinGW_32bit-Debug\\training_images\\images.bin";
     const char* labelsFile = "C:\\Users\\Yulia\\Documents\\Informatik\\WS18-19\\PRG_Praktikum\\milestones\\build-milestone4-Desktop_Qt_5_9_2_MinGW_32bit-Debug\\training_images\\images.labels";
 
@@ -26,7 +23,7 @@ int main(int argc, char *argv[])
 
     if (stat(labelsFile, &results) == 0)
         // The size of the file in bytes is in results.st_size
-        cout << results.st_size << endl;
+        cout << "File size in Bytes: " << results.st_size << endl;
     else
         // An error occurred
         cout << "Error" << endl;
@@ -51,6 +48,9 @@ int main(int argc, char *argv[])
     }
     cout << endl;
 
+    int32_t numImages = buffer[7] | buffer[6]<<8 | buffer[5]<<16 | buffer[4]<<24;
+    cout << "Number of images: " << numImages << endl;
+
     if (!myFile) {
         // An error occurred!
         // myFile.gcount() returns the number of bytes read.
@@ -58,7 +58,6 @@ int main(int argc, char *argv[])
         // so it is usable again.
         cout << "Error" << endl;
     }
-    //cout << to_string(buffer[2]) << endl;
 
     //return 0;
     return a.exec();
