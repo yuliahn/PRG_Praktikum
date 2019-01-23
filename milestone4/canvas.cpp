b6be2439b94d8746b705011542ee1c5d3cdc92a2
ToniOs1996 <44807471+ToniOs1996@users.noreply.github.com>
2019-01-23 22:45:32 +0100

    Update canvas.cpp

--- milestone4/canvas.cpp
+++ milestone4/canvas.cpp
@@ -11,8 +11,8 @@ Canvas::Canvas(QWidget *parent) :
     ui(new Ui::Canvas)
 {
     ui->setupUi(this);
-    this->x_pixel_amount = ui->rowsPixel->value();
-    this->y_pixel_amount = ui->columnsPixel->value();
+    this->xPixel = ui->rowsPixel->value();
+    this->yPixel = ui->columnsPixel->value();
 
 
 }
@@ -25,7 +25,7 @@ Canvas::~Canvas()
 void Canvas::on_setButton_clicked()
 {
     cout << "im here";
-    //setCanvas(x_pixel_amount, y_pixel_amount);
+    setCanvas(xPixel, yPixel);
     cout << "Test2";
     //update();
 }
@@ -33,6 +33,7 @@ void Canvas::on_setButton_clicked()
 void Canvas::on_clearButton_clicked()
 {
     cout<<"clear button";
+    setCanvas(xPixel, yPixel);
 }
 
 vector<vector<double>> Canvas::setCanvas(int x, int y)
@@ -60,12 +61,17 @@ void Canvas::setPixel(int i, int j)
 }
 
 void Canvas::paintEvent(QPaintEvent * event) {
+    emit environmentChanged(true);
     QPainter p(this);
+    setCanvas(xPixel, yPixel);
     createField(p);
+    paintField(p);
 }
 
 void Canvas::mousePressEvent(QMouseEvent *event) {
-
+    double x = event->x();
+    double y = event->y();
+    setPixel(x/xPixel, y/yPixel);
     update();
 }
 
@@ -75,23 +81,49 @@ void Canvas::mousePressEvent(QMouseEvent *event) {
  * @param event
  */
 void Canvas::mouseMoveEvent(QMouseEvent *event) {
-
+    double x = event->x();
+    double y = event->y();
+    setPixel(x/xPixel, y/yPixel);
     update();
 }
 
 void Canvas::createField(QPainter &p){
-    int size = 500;
+    int size = 400;
     QRect borders(0, 0, size, size); // borders of the universe
     QColor gridColor = "#000"; // color of the grid
     gridColor.setAlpha(20); // must be lighter than main color
     p.setPen(gridColor);
-    double cellWidth = (double) size / 100 ; // width of the widget / number of cells at one row
+    double cellWidth = (double) size / xPixel ; // width of the widget / number of cells at one row
     for (double k = cellWidth; k <= size; k += cellWidth)
         p.drawLine(k, 0, k, size);
-    double cellHeight = (double) size / 100; // height of the widget / number of cells at one row
+    double cellHeight = (double) size / yPixel; // height of the widget / number of cells at one row
     for (double k = cellHeight; k <= size; k += cellHeight)
         p.drawLine(0, k, size, k);
     p.drawRect(borders);
+}
+
+void Canvas::paintField(QPainter &p) {
+    int size = 400;
+    double cellWidth = (double) size / xPixel ;
+    double cellHeight = (double) size / yPixel ;
+    for (int i = 0; i < xPixel; i++) {
+        for (int j = 0; j < yPixel; j++) {
+            if (canvas[i][j]==1){
+                QRectF r(i*cellWidth, j*cellHeight, cellWidth, cellHeight);
+                p.fillRect(r, QBrush("#1e1b18"));
+            }else if (canvas[i][j]==0.75){
+                QRectF r(i*cellWidth, j*cellHeight, cellWidth, cellHeight);
+                p.fillRect(r, QBrush("#868482"));
+            }else if (canvas[i][j]==0.5){
+                QRectF r(i*cellWidth, j*cellHeight, cellWidth, cellHeight);
+                p.fillRect(r, QBrush("#aeadac"));
+            }else if (canvas[i][j]==0.25){
+                QRectF r(i*cellWidth, j*cellHeight, cellWidth, cellHeight);
+                p.fillRect(r, QBrush("#d7d6d5"));
+            }
+
+        }
+    }
 }
 
 
