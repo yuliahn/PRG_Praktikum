992e455b318d82e7f196862c5fcc76ee6c44a269
ToniOs1996 <44807471+ToniOs1996@users.noreply.github.com>
2019-01-23 12:36:51 +0100

    -Button Clicks added -Pixel amout X and Y added

--- milestone4/canvas.h
+++ milestone4/canvas.h
@@ -20,10 +20,27 @@ public:
 
     vector<vector<double>> setCanvas(int x, int y);
     void setPixel(int i, int j);
+    void rowsPixel(double value);
+    void columnsPixel(double value);
 
+    unsigned getSizeOfCanvas {this->canvas.size()};
+
+protected:
+    void paintEvent(QPaintEvent *);
+    void mousePressEvent(QMouseEvent *e);
+    void mouseMoveEvent(QMouseEvent *e) override;
+    void createField(QPainter &p);
+
+private slots:
+    void on_setButton_clicked();
+
+    void on_clearButton_clicked();
+    
 private:
     Ui::Canvas *ui;
     vector<vector<double>> canvas;
+    int x_pixel_amount;
+    int y_pixel_amount;
 
 };
 
