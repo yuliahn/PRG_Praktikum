c77ccd548d76d5577d815209fdea3a20b05f6445
yuliahn <38583886+yuliahn@users.noreply.github.com>
2019-01-25 18:33:38 +0100

    Update main.cpp

--- milestone4/main.cpp
+++ milestone4/main.cpp
@@ -9,47 +9,43 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-
+    /*
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-
+    */
 
     const char* imagesFile = "C:\\Users\\Yulia\\Documents\\Informatik\\WS18-19\\PRG_Praktikum\\milestones\\build-milestone4-Desktop_Qt_5_9_2_MinGW_32bit-Debug\\training_images\\images.bin";
     const char* labelsFile = "C:\\Users\\Yulia\\Documents\\Informatik\\WS18-19\\PRG_Praktikum\\milestones\\build-milestone4-Desktop_Qt_5_9_2_MinGW_32bit-Debug\\training_images\\images.labels";
 
-    // http://courses.cs.vt.edu/~cs2604/fall00/binio.html
-    struct stat results;
-
-    if (stat(labelsFile, &results) == 0)
-        // The size of the file in bytes is in results.st_size
-        cout << "File size in Bytes: " << results.st_size << endl;
-    else
-        // An error occurred
-        cout << "Error" << endl;
-
-    char* buffer;
-    buffer = new char[101];
-
-
     ifstream myFile (imagesFile, ios::in | ios::binary);
-    myFile.read (buffer, 101);
+
+    int length;
+    myFile.seekg (0, ios::end);
+    length = myFile.tellg();
+    myFile.seekg (0, ios::beg);
+
+    cout << "File size in Bytes: " << length << endl;
+
+    char * buffer;
+    buffer = new char[length];
+
+    myFile.read (buffer, length);
+
     cout << myFile.gcount() << " Bytes were read." << '\n' << endl;
 
-    /*
-    for (int i = 0; i < 101; i++) {
-        cout << ' ' << buffer[i];
-    }
-    cout << endl;
-    */
+    // 60 000 labels => 60 000 images
 
-    for (int i = 0; i < 101; i++) {
+    for (int i = 0; i < 20; i++) {
         cout << ' ' << to_string(buffer[i]);
     }
     cout << endl;
 
-    int32_t numImages = buffer[7] | buffer[6]<<8 | buffer[5]<<16 | buffer[4]<<24;
+    //int32_t numImages = buffer[7] | buffer[6]<<8 | buffer[5]<<16 | buffer[4]<<24;
+    unsigned int numImages = buffer[7] | buffer[6]<<8 | buffer[5]<<16 | buffer[4]<<24;
     cout << "Number of images: " << numImages << endl;
+
+    // from buffer[8] till the end: pixel data
 
     if (!myFile) {
         // An error occurred!
@@ -59,6 +55,6 @@ int main(int argc, char *argv[])
         cout << "Error" << endl;
     }
 
-    //return 0;
-    return a.exec();
+    return 0;
+    //return a.exec();
 }
