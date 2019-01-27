32820154431c3ccb063b321802a13d648a10bda1
yuliahn <38583886+yuliahn@users.noreply.github.com>
2019-01-27 20:06:28 +0100

    Update aufgabe2.h

--- milestone4/aufgabe2.h
+++ milestone4/aufgabe2.h
@@ -4,9 +4,13 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
 char * importFile(string fileName);
+vector<vector<vector<unsigned int>>> copyData(char * buffer);
+vector<vector<unsigned int>> copyLabels(char * buffer);
+
 
 #endif // AUFGABE2_H
