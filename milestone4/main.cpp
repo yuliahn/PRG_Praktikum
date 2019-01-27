c06ea53651af3cc9de75646e786a678dacffcc12
yuliahn <38583886+yuliahn@users.noreply.github.com>
2019-01-27 13:41:53 +0100

    Update main.cpp

--- milestone4/main.cpp
+++ milestone4/main.cpp
@@ -1,10 +1,14 @@
 #include "mainwindow.h"
+#include "NeuralNet/neuralnet.h"
+#include "aufgabe2.cpp"
+
 #include <QApplication>
 
-#include <sys/stat.h>
 #include <iostream>
 #include <fstream>
 #include <string.h>
+#include <vector>
+
 using namespace std;
 
 int main(int argc, char *argv[])
@@ -18,42 +22,85 @@ int main(int argc, char *argv[])
     const char* imagesFile = "C:\\Users\\Yulia\\Documents\\Informatik\\WS18-19\\PRG_Praktikum\\milestones\\build-milestone4-Desktop_Qt_5_9_2_MinGW_32bit-Debug\\training_images\\images.bin";
     const char* labelsFile = "C:\\Users\\Yulia\\Documents\\Informatik\\WS18-19\\PRG_Praktikum\\milestones\\build-milestone4-Desktop_Qt_5_9_2_MinGW_32bit-Debug\\training_images\\images.labels";
 
-    ifstream myFile (imagesFile, ios::in | ios::binary);
+    cout << "Importing data from images..." << endl;
+    char* buffer = importFile(imagesFile);
 
-    int length;
-    myFile.seekg (0, ios::end);
-    length = myFile.tellg();
-    myFile.seekg (0, ios::beg);
-
-    cout << "File size in Bytes: " << length << endl;
-
-    char * buffer;
-    buffer = new char[length];
-
-    myFile.read (buffer, length);
-
-    cout << myFile.gcount() << " Bytes were read." << '\n' << endl;
-
-    // 60 000 labels => 60 000 images
-
-    for (int i = 0; i < 20; i++) {
+    /*
+    for (int i = 0; i < 100; i++) {
         cout << ' ' << to_string(buffer[i]);
     }
     cout << endl;
+    */
 
-    //int32_t numImages = buffer[7] | buffer[6]<<8 | buffer[5]<<16 | buffer[4]<<24;
-    unsigned int numImages = buffer[7] | buffer[6]<<8 | buffer[5]<<16 | buffer[4]<<24;
-    cout << "Number of images: " << numImages << endl;
+    cout << "Copying data..." << endl;
+    unsigned int iterator = 16;
+    vector<vector<vector<unsigned int>>> data;
 
-    // from buffer[8] till the end: pixel data
-
-    if (!myFile) {
-        // An error occurred!
-        // myFile.gcount() returns the number of bytes read.
-        // calling myFile.clear() will reset the stream state
-        // so it is usable again.
-        cout << "Error" << endl;
+    for (unsigned int batch = 0; batch < 120; batch++) {
+        //cout << "Copying batch " << batch+1 << "..." << endl;
+        vector<vector<unsigned int>> batchVec;
+        for (unsigned int image = 0; image < 500; image++) {
+            //cout << "   Copying image " << image+1 << endl;
+            vector<unsigned int> imageVec;
+            for (unsigned int pixel = 0; pixel < 784; pixel++) {
+                unsigned char newPixel = buffer[iterator++];
+                unsigned int px = newPixel;
+                //cout << "      Copying pixel " << pixel+1 << ": " << px << endl;
+                imageVec.push_back(px);
+            }
+            batchVec.push_back(imageVec);
+        }
+        data.push_back(batchVec);
     }
+
+    cout << "Copied batches: " << data.size() << endl;
+
+
+    cout << "\nImporting data from labels..." << endl;
+    char* labelsBuffer = importFile(labelsFile);
+
+    cout << "Copying labels..." << endl;
+    unsigned int LabelsIterator = 8;
+    vector<vector<unsigned int>> labels;
+
+    for (unsigned int batch = 0; batch < 120; batch++) {
+        //cout << "Copying batch " << batch+1 << "..." << endl;
+        vector<unsigned int> batchVec;
+        for (unsigned int image = 0; image < 500; image++) {
+            //cout << "   Copying image " << image+1 << endl;
+            unsigned char newImage = labelsBuffer[LabelsIterator++];
+            unsigned int img = newImage;
+            batchVec.push_back(img);
+        }
+        labels.push_back(batchVec);
+    }
+    cout << "Copied labels: " << LabelsIterator-8 << endl;
+    cout << "Copied batches: " << labels.size() << endl;
+
+    vector<int> topology = {784,64,10};
+    double eta = 10; //0.3
+    double alpha = 0;
+    NeuralNet net(topology);
+    vector<double> values(10,0);
+
+    vector <double> output = net.getOutput();
+
+    // Train the network with batch 1:
+    for (unsigned int image = 0; image < 500; image++) { // 500 images
+        values[labels[0][image]] = 1000000; // rescale from 1 to 1000000
+        net.setInput(data[0][image]);
+        net.back(eta, alpha, values);
+
+        output = net.getOutput();
+        //cout << "\nOutput: ";
+        //net.printOutput();
+    }
+
+
+    net.setInput(data[0][1]);
+    cout << "\nTest " << labels[0][1] << ": ";
+    net.printOutput();
+
 
     return 0;
     //return a.exec();
