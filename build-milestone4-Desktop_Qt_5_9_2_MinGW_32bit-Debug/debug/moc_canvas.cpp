138327bf072f9e800d7085f4bc96f01f0805b9de
yuliahn <38583886+yuliahn@users.noreply.github.com>
2019-01-28 14:18:14 +0100

    exe files

--- build-milestone4-Desktop_Qt_5_9_2_MinGW_32bit-Debug/debug/moc_canvas.cpp
+++ build-milestone4-Desktop_Qt_5_9_2_MinGW_32bit-Debug/debug/moc_canvas.cpp
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_Canvas_t {
-    QByteArrayData data[8];
-    char stringdata0[102];
+    QByteArrayData data[10];
+    char stringdata0[160];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -38,12 +38,15 @@ QT_MOC_LITERAL(3, 27, 2), // "ok"
 QT_MOC_LITERAL(4, 30, 7), // "TSPends"
 QT_MOC_LITERAL(5, 38, 20), // "on_setButton_clicked"
 QT_MOC_LITERAL(6, 59, 22), // "on_clearButton_clicked"
-QT_MOC_LITERAL(7, 82, 19) // "on_testData_clicked"
+QT_MOC_LITERAL(7, 82, 19), // "on_testData_clicked"
+QT_MOC_LITERAL(8, 102, 27), // "on_importDataButton_clicked"
+QT_MOC_LITERAL(9, 130, 29) // "on_importLabelsButton_clicked"
 
     },
     "Canvas\0environmentChanged\0\0ok\0TSPends\0"
     "on_setButton_clicked\0on_clearButton_clicked\0"
-    "on_testData_clicked"
+    "on_testData_clicked\0on_importDataButton_clicked\0"
+    "on_importLabelsButton_clicked"
 };
 #undef QT_MOC_LITERAL
 
@@ -53,7 +56,7 @@ static const uint qt_meta_data_Canvas[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       5,   14, // methods
+       7,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -61,19 +64,23 @@ static const uint qt_meta_data_Canvas[] = {
        2,       // signalCount
 
  // signals: name, argc, parameters, tag, flags
-       1,    1,   39,    2, 0x06 /* Public */,
-       4,    1,   42,    2, 0x06 /* Public */,
+       1,    1,   49,    2, 0x06 /* Public */,
+       4,    1,   52,    2, 0x06 /* Public */,
 
  // slots: name, argc, parameters, tag, flags
-       5,    0,   45,    2, 0x08 /* Private */,
-       6,    0,   46,    2, 0x08 /* Private */,
-       7,    0,   47,    2, 0x08 /* Private */,
+       5,    0,   55,    2, 0x08 /* Private */,
+       6,    0,   56,    2, 0x08 /* Private */,
+       7,    0,   57,    2, 0x08 /* Private */,
+       8,    0,   58,    2, 0x08 /* Private */,
+       9,    0,   59,    2, 0x08 /* Private */,
 
  // signals: parameters
     QMetaType::Void, QMetaType::Bool,    3,
     QMetaType::Void, QMetaType::Bool,    3,
 
  // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -92,6 +99,8 @@ void Canvas::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void
         case 2: _t->on_setButton_clicked(); break;
         case 3: _t->on_clearButton_clicked(); break;
         case 4: _t->on_testData_clicked(); break;
+        case 5: _t->on_importDataButton_clicked(); break;
+        case 6: _t->on_importLabelsButton_clicked(); break;
         default: ;
         }
     } else if (_c == QMetaObject::IndexOfMethod) {
@@ -139,13 +148,13 @@ int Canvas::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 5)
+        if (_id < 7)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 5;
+        _id -= 7;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 5)
+        if (_id < 7)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 5;
+        _id -= 7;
     }
     return _id;
 }
