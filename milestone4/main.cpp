df56de2479d87774d6387744c676efb06b6971d0
yuliahn <38583886+yuliahn@users.noreply.github.com>
2019-01-28 14:17:23 +0100

    Update main.cpp

--- milestone4/main.cpp
+++ milestone4/main.cpp
@@ -14,11 +14,11 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    /*
+     /*
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-    */
+     */
 
     // /*
     const char* imagesFile = "C:\\Users\\Yulia\\Documents\\Informatik\\WS18-19\\PRG_Praktikum\\milestones\\build-milestone4-Desktop_Qt_5_9_2_MinGW_32bit-Debug\\training_images\\images.bin";
@@ -28,9 +28,25 @@ int main(int argc, char *argv[])
     char* buffer = importFile(imagesFile);
     vector<vector<vector<unsigned int>>> data = copyData(buffer);
 
+    /*
+    for (unsigned int image = 0; image < 20; image++) {
+        vector <unsigned int> testData = data[0][image];
+        unsigned int counter = 0;
+        for (int i = 0; i < 28; i++) {
+            for (int j = 0; j < 28; j++) {
+                unsigned int value = testData[counter++];
+                cout << value << ' ';
+            }
+            cout << endl;
+        }
+        cout << '\n' << endl;
+    }
+    */
+
     cout << "\nImporting data from labels..." << endl;
     char* labelsBuffer = importFile(labelsFile);
     vector<vector<unsigned int>> labels = copyLabels(labelsBuffer);
+
 
     vector<int> topology = {784,64,10};
     double eta = 0.3; //0.3
@@ -40,7 +56,7 @@ int main(int argc, char *argv[])
     vector <double> output = net.getOutput();
 
     double batchError = net.trainBatch(data, labels, eta, alpha, 0);
-    for (unsigned int batch = 1; batch < 2; batch++) { //120 batches
+    for (unsigned int batch = 1; batch < 120; batch++) { //120 batches
         cout << "\nTraining batch " << batch << "..." << endl;
         double curBatchError = net.trainBatch(data, labels, eta, alpha, batch);
         if (curBatchError - batchError <= 0.01) {
@@ -56,7 +72,9 @@ int main(int argc, char *argv[])
     cout << "\nTest " << labels[0][2] << ": ";
     net.printOutput();
 
+    net.exportState("trainedANN.txt"); // start: 13:29
+
     return 0;
-    //*/
+    // */
     //return a.exec();
 }
